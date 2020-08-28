083c90a28fcf361d46803673b11715c1922b2e9f
iandemed <iandemed@gmail.com>
2020-08-28 00:34:53 -0400

    Updates

---
+++ precache-manifest.4297982ad07a4ddcf8262572c9307c92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ccd475a77be93f14718bd5ad73131ef",
+    "url": "/dnd-react-ui/index.html"
+  },
+  {
+    "revision": "c566540e4fe1e1f552e1",
+    "url": "/dnd-react-ui/static/css/main.4780dc7e.chunk.css"
+  },
+  {
+    "revision": "2e7aa4124413c89c1702",
+    "url": "/dnd-react-ui/static/js/2.a6399649.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dnd-react-ui/static/js/2.a6399649.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c566540e4fe1e1f552e1",
+    "url": "/dnd-react-ui/static/js/main.7b6db2ec.chunk.js"
+  },
+  {
+    "revision": "d8105d1ef14801307b0e",
+    "url": "/dnd-react-ui/static/js/runtime-main.fad26573.js"
+  }
+]);
