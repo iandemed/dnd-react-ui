ff0b21bf7790a9e0d6fc495b7b4fcc7c2c72e3bb
iandemed <iandemed@gmail.com>
2020-08-28 14:39:51 -0400

    Fixed error where blank proficiences were being shown

--- src/Proficiencies.js
+++ src/Proficiencies.js
@@ -5,32 +5,32 @@ import './CharacterSheet.css'
 
 const Proficiencies = ({type, proficiencies}) => {
 
+
+    let typeProficiencies = proficiencies.filter( (obj) => {
+        return obj.name.includes(type)  
+    })
+
     function prepareProficencies(type, proficiencies){
         
-        let typeProficiencies = proficiencies.filter( (obj) => {
-  
-            return obj.name.includes(type)
-            
-        })
 
         let monsterProficiencies = ""
 
-        for(let i = 0;  i < typeProficiencies.length; i++){
-            monsterProficiencies += `${typeProficiencies[i].name.replace(type,'')}+${typeProficiencies[i].value} `
+        for(let i = 0;  i < proficiencies.length; i++){
+            monsterProficiencies += `${proficiencies[i].name.replace(type,'')}+${proficiencies[i].value} `
         }
 
         return monsterProficiencies.split(0, -1)
     
     }
 
-    if (proficiencies.length === 0){
+    if ( typeProficiencies.length === 0){
         return(null)
     }
     else {
         return(
 
         <div>
-           <strong>{type}</strong> {prepareProficencies(type, proficiencies)}
+           <strong>{type}</strong> {prepareProficencies(type, typeProficiencies)}
         </div>
 
         )
